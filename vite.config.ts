6aae72befb653e0fa0c926426a62e0eb6cd810ce
xieisabug <136419808@qq.com>
2024-09-30 20:58:32 +0800

    clean project

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: resolve(__dirname, 'src/main.tsx'),
+      output: {
+        entryFileNames: 'main.js',
+        format: 'es',
+      },
+    },
+  },
 })
