486b474bedbb4ec79d9cbeb3fc6cfa9406fb3ea6
xieisabug <136419808@qq.com>
2024-10-01 15:54:37 +0800

    Refactor build configuration in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,14 +4,20 @@ import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    rollupOptions: {
-      input: resolve(__dirname, 'src/main.tsx'),
-      output: {
-        entryFileNames: 'main.js',
-        format: 'es',
-      },
-    },
-  },
+	plugins: [react()],
+	build: {
+		lib: {
+			entry: resolve(__dirname, 'src/main.tsx'),
+			name: 'SamplePlugin',
+			fileName: 'main',
+			formats: ['es']
+		},
+		rollupOptions: {
+			input: resolve(__dirname, 'src/main.tsx'),
+			output: {
+				entryFileNames: 'main.js',
+				format: 'es',
+			},
+		},
+	},
 })
