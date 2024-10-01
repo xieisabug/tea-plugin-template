e39610db38d16b15cc49c5c0ca6f417ff1fe7d49
xieisabug <136419808@qq.com>
2024-10-01 23:02:53 +0800

    Refactor package.json to add @rollup/plugin-replace as a dev dependency

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { resolve } from 'path'
+import replace from '@rollup/plugin-replace'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
+	plugins: [
+		react(),
+		replace({
+			'process.env.NODE_ENV': JSON.stringify('production'),
+			preventAssignment: true
+		})
+	],
 	build: {
 		lib: {
 			entry: resolve(__dirname, 'src/main.tsx'),
@@ -13,7 +20,6 @@ export default defineConfig({
 			formats: ['es']
 		},
 		rollupOptions: {
-			input: resolve(__dirname, 'src/main.tsx'),
 			output: {
 				entryFileNames: 'main.js',
 				format: 'es',
